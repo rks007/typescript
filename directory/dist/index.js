2f6dfb554cc0a9784f36ffde5d3e69d83038aa32
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2024-06-13 00:34:19 +0530

    add more type script

--- directory/dist/index.js
+++ directory/dist/index.js
@@ -3,3 +3,12 @@ let age = 5;
 if (age > 1) {
     console.log("hi there");
 }
+const data = {
+    name: "kiba",
+    age: 12,
+    username: "kibakai@gmail.com"
+};
+function takeData(user) {
+    console.log(user.age);
+}
+takeData(data);
--- directory/src/index.ts
+++ directory/src/index.ts
@@ -3,3 +3,21 @@ if(age > 1){
     console.log("hi there");
     
 }
+
+interface user {
+    name: string,
+    age: number,
+    username: string
+}
+
+const data: user = {
+    name: "kiba",
+    age: 12,
+    username: "kibakai@gmail.com"
+}
+
+function takeData(user: user){
+    console.log(user.age);
+}
+
+takeData(data);
